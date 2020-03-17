e687489e4fd6ff8168cfdd910ba00ef265fc5590
Theo Dias Mitchell <theomitchell@gmail.com>
2020-03-17 13:22:47 -0400

    Initial Commit

---
+++ Lesson1A-VS/Scripts/app.js
@@ -0,0 +1,11 @@
+﻿// IIFE - Immediatly Invoke Function Expression
+(function () {
+    function Start() {
+        console.log("App Started!");
+        console.info("---------------");
+        console.warn("This is an error!");
+
+    }
+
+    window.onload = Start;
+})();
